29808b7615d6d588e48262b7fb430dd185f4151a
lee <a31257122@gmail.com>
2023-05-03 17:40:58 +0900

    20230503

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_portfolio/my_dice.dart';
 import 'package:flutter_application_portfolio/my_timer.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -37,20 +38,33 @@ class _MyAppState extends State<MyApp> {
     // TODO: implement initState
     super.initState();
     items.add(BottomNavigationBarItem(
-        icon: Icon(Icons.home), label: '홈', backgroundColor: Colors.amber));
+        icon: Icon(Icons.home),
+        activeIcon: Icon(Icons.home, color: Colors.orange),
+        label: '홈',
+        backgroundColor: Colors.amber));
     items.add(BottomNavigationBarItem(
-        icon: Icon(Icons.access_time_rounded),
+        icon: Icon(
+          Icons.access_time_rounded,
+        ),
+        activeIcon: Icon(Icons.access_time_rounded, color: Colors.amber),
         label: '타이머',
         backgroundColor: Colors.green));
     items.add(BottomNavigationBarItem(
         icon: Icon(Icons.check_box_outlined),
+        activeIcon: Icon(
+          Icons.check_box_outlined,
+          color: Colors.brown,
+        ),
         label: '뽑기',
         backgroundColor: Colors.black));
     items.add(BottomNavigationBarItem(
-        icon: Icon(Icons.message), label: '소개', backgroundColor: Colors.blue));
+        icon: FaIcon(FontAwesomeIcons.user),
+        activeIcon: FaIcon(FontAwesomeIcons.user, color: Colors.grey),
+        label: '소개',
+        backgroundColor: Colors.blue));
     //첫페이지를 지정
     bodyPage = Center(
-      child: Text('홈임시'),
+      child: FaIcon(FontAwesomeIcons.dAndDBeyond, size: 40),
     );
   }
 
